70e50db262eb03d7e9cea5d68472ba7818f961de
Lydia Garcia <lydia.garcia@philips.com>
2022-04-16 18:51:00 -0300

    layout fixes, button to whatsapp done

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,19 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function enableButton(buttonId, nameId){
+
+	if(window.document.getElementById(nameId).value != ''){
+		window.document.getElementById(buttonId).disabled = false;
+	} else{
+		window.document.getElementById(buttonId).disabled = true;
+	}
+}
+
+function openWhatsApp(nameId){
+	var name = window.document.getElementById(nameId).value;
+	var link = 'https://api.whatsapp.com/send?phone=47999340994&text=Ol%C3%A1,%20me%20chamo%20#name#%20e%20gostaria%20de%20saber%20mais%20sobre%20a%20Atma%20Terapias.';
+	var replaced_link = link.replace('#name#', name);
+
+	window.open(replaced_link);
+}
